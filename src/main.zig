e7bfd2dcf7e13fe5b8d3de74b6c18f0297321c98
wbmcglaughlin <wmcg0001@student.monash.edu>
2023-12-21 08:58:26 +1100

    refa: main.py

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,13 @@
-const std = @import("std");
+const r = @cImport(@cInclude("raylib.h"));
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    r.InitWindow(960, 540, "My Window Name");
+    r.SetTargetFPS(144);
+    defer r.CloseWindow();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    while (!r.WindowShouldClose()) {
+        r.BeginDrawing();
+        r.ClearBackground(r.BLACK);
+        r.EndDrawing();
+    }
 }
