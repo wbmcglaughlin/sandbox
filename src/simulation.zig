7194d9c25b4fb454203a2a0225a41cf34e6b82dc
wbmcglaughlin <wmcg0001@student.monash.edu>
2023-12-23 20:59:42 +1100

    feat: rng liquid interaction

--- src/simulation.zig
+++ src/simulation.zig
@@ -21,8 +21,20 @@ pub fn swap(points: *Points, x1: usize, y1: usize, x2: usize, y2: usize) void {
 }
 
 pub fn up_pass(points: *Points) !void {
+    var prng = std.rand.DefaultPrng.init(blk: {
+        var seed: u64 = undefined;
+        try std.os.getrandom(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
+    var non_empty: u32 = 0;
     for (0..window.WIDTH) |x| {
-        for (1..(window.HEIGHT - 1)) |y| {
+        for (1..(window.HEIGHT - 1)) |y_a| {
+            const y = window.HEIGHT - y_a - 1;
+            if (points[x][y] == ParticleType.empty) {
+                continue;
+            }
+
+            non_empty += 1;
             // The effect of gravity on a solid particle.
             if (points[x][y + 1] == ParticleType.empty) {
                 swap(points, x, y, x, y + 1);
@@ -32,8 +44,23 @@ pub fn up_pass(points: *Points) !void {
                     swap(points, x, y, x - 1, y + 1);
                 } else if (points[x + 1][y + 1] == ParticleType.empty and x != window.WIDTH - 1) {
                     swap(points, x, y, x + 1, y + 1);
+                } else {
+                    // At this point there is no space for the particle to fall down into.
+                    // Liquids should randomly move left or right if there is available space.
+                    const value = prng.random().intRangeAtMost(i64, 0, 1);
+                    if (value == 0) {
+                        if (points[x - 1][y] == ParticleType.empty and x != 0) {
+                            swap(points, x, y, x - 1, y);
+                        }
+                    } else {
+                        if (points[x + 1][y] == ParticleType.empty and x != window.WIDTH - 1) {
+                            swap(points, x, y, x + 1, y);
+                        }
+                    }
                 }
             }
         }
     }
+
+    std.debug.print("{}\n", .{non_empty});
 }
